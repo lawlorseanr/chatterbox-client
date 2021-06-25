56b474250fd1e8ccc721470e38edc8132647ac93
Sean Lawlor <lawlorseanr@gmail.com>
2021-06-25 09:57:42 -0700

    Implement Parse.create() in parse.js

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,7 +3,20 @@ var Parse = {
   server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
 
   create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
+    $.ajax({
+      // This is the url you should use to communicate with the API server.
+      url: 'https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/hr-lax',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
