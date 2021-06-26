9cf6a311864ecdf67e1b96196f5e034c4da853b6
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-06-25 17:00:19 -0700

    Filtered messages to exclude attacks or blank messages

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -9,6 +9,14 @@ var MessagesView = {
     // reset element
     this.$chats.html('');
     for (var i = 0; i < Messages.storage.length; i++) {
+      // stringify message
+      var message = JSON.stringify(Messages.storage[i]);
+      // if there's no undesirable text
+      // want length > 0; don't want  script or style tags
+      var valid = Messages.storage[i].text.length > 0 && Messages.storage[i].username.length > 0;
+      if (message.indexOf('script') >= 0 || message.indexOf('style') >= 0 || !valid) {
+        continue;
+      }
       MessagesView.renderMessage(Messages.storage[i]);
     }
     // messages.forEach(message => MessagesView.$chats.append(MessageView.render(message)));
