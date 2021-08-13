ad99316062f2bd08b4185f5ae1a49612d686bcd6
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-13 10:12:43 -0700

    Incorporate notes about removing escaping to view changes

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -5,8 +5,20 @@ var MessageView = {
         <div class="username" data-username="<%- github %>">
           <%- username %> (<%- github %>):
         </div>
-        <div class="messageText"><%- text %></div>
+        <div class="messageText"><%= text %></div>
         <div class="timestamp"><%- date %></div>
       </div>
     `),
 };
+
+// without escaping:
+
+// render: _.template(`
+// <div class="chat<%- Friends.isFriend(github) ? ' friend' : '' %>">
+//   <div class="username" data-username="<%- github %>">
+//     <%- username %> (<%- github %>):
+//   </div>
+//   <div class="messageText"><%- text %></div>
+//   <div class="timestamp"><%- date %></div>
+// </div>
+// `),
